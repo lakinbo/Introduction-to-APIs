19b2cd4a8eb4aa0c3a8857d3a60fa6680e394169
Lanre Akinbo <lanre.akinbo@gmail.com>
2025-10-22 23:31:56 +0100

    modified 'notes'

--- notes.py
+++ notes.py
@@ -1,37 +1,31 @@
 # API - Application Programming Interface
 
-# Request types and basic functionality of a rest api is to be able to
-# CRUD functionality - Create, Read, Update, Delete
+# Request types and basic functionality of a rest api is to be able to Create, Read, Update, Delete
+# CRUD functionality
 
 # POST - to create data in our DB
-
 # GET - to get data from our DB
-
 # PUT - to update data in our DB
-
 # DELETE - to delete data from our database
 
 # Creating Virtual Environment
-# windows:
-# python -m venv venv
-# Mac:
-# python3 -m venv venv
+    # Windows: 
+    # python -m venv venv
 
-# Activation Virtual Environment
-# Windows
-# venv\Scripts\activate
+    # Mac:
+    # python3 -m venv venv
 
-# Mac
-# source venv/bin/activate
+# Activation of Virtual Environment
+    # Windows
+    # venv\Scripts\activate
 
-# Confirm active bu seeing (venv) next to user in terminal
+    # Mac
+    # source venv/bin/activate
 
+# Confirm active by seeing (venv) next to user in terminal
 
 # Installing...........
-
-# pip install requests
-
+    # pip install requests
 
 # Check installs...........
-
-# pip list (lists out all installed packages)
+    # pip list (lists out all installed packages)
